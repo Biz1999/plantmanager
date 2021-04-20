93b6b0703bf09895441e18a1e253d7d3fc5161df
Alessandro <alessanbiz@gmail.com>
2021-04-20 16:35:21 -0300

    Navigation and initial pages
    Finishing initial pages with navigation

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,38 +1,42 @@
 import React from 'react';
-import { Text, TouchableOpacity, StyleSheet, TouchableOpacityProps } from 'react-native';
+import {
+    StyleSheet,
+    Text,
+    TouchableOpacity,
+    TouchableOpacityProps
+} from 'react-native';
+
 import colors from '../styles/colors';
+import fonts from '../styles/fonts';
 
 interface ButtonProps extends TouchableOpacityProps {
     title: string;
 }
+export default function Button({ title, ...rest }: ButtonProps) {
 
-export default function Button({ title, ...rest } : ButtonProps){
-    return(       
-        <TouchableOpacity 
-        style={styles.button} 
-        activeOpacity={0.7}
-        {...rest}
+    return (
+        <TouchableOpacity
+            style={styles.container}
+            {...rest}
         >
-            <Text style={styles.buttonText}>
+            <Text style={styles.text}>
                 {title}
-                
             </Text>
         </TouchableOpacity>
     )
 }
 
 const styles = StyleSheet.create({
-    button: {
+    container: {
         backgroundColor: colors.green,
+        height: 56,
+        borderRadius: 16,
         justifyContent: 'center',
         alignItems: 'center',
-        borderRadius: 16,
-        marginBottom: 10,
-        height: 56,
-        width: 56
     },
-    buttonText: {
+    text: {
+        fontSize: 16,
         color: colors.white,
-        fontSize: 24,
+        fontFamily: fonts.heading
     }
 })
